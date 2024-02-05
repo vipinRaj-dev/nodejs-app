af4956f04b0e824b110c135c8e08f661848bf498
vipin <vipinrj8473@gmail.com>
2024-02-05 10:53:34 +0530

    last

--- mongoC.js
+++ mongoC.js
@@ -1,14 +1,13 @@
 import { MongoClient } from "mongodb";
 
-// const password = encodeURIComponent(process.env.MONGO_PASSWORD.trim());
 const connectionString = 'mongodb+srv://vipinraj19060057:Vipin9645@devcluster.uayvlsc.mongodb.net/?retryWrites=true&w=majority'
 const client = new MongoClient(connectionString);
 let conn;
 try {
   conn = await client.connect();
   console.log("connection successful")
-} catch(e) {
+} catch(e) {  
   console.error(e);
 }
 let db = conn.db("userdetails");
-export default db;
+export default db;  
